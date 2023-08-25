55fb9f5f0162c6f4ca1023cfdd36f47e51ab27d9
PedroLMaia <pedro-luiz-maia@hotmail.com>
2023-08-25 14:17:05 -0300

    drop table, usando novos tipos

---
+++ tipos-dados/tipo-numerico.sql
@@ -0,0 +1,19 @@
+DROP TABLE aluno;
+
+CREATE TABLE aluno (
+   matricula CHAR(10) NOT NULL,
+   nome VARCHAR(120) NOT NULL,
+   nome_social VARCHAR(80),
+   semestre TINYINT,
+   creditos_cursados SMALLINT,
+   media_geral DECIMAL(4,2)
+)
+
+INSERT INTO aluno
+VALUES(
+    '202106A6F7', -- matricula, 
+    'Marlos Pinheiro Borges', -- nome
+    'Marlin' -- nome_social
+);
+
+SELECT * FROM aluno;
