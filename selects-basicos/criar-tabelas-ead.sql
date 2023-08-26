47da1ea61cac91e1508fe9a197cc59049d0949a0
PedroLMaia <pedro-luiz-maia@hotmail.com>
2023-08-26 17:31:03 -0300

    tabela cruso

---
+++ selects-basicos/criar-tabelas-ead.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS curso;
+
+CREATE TABLE IF NOT EXISTS curso(
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(120) NOT NULL,
+    descricao VARCHAR(4000),
+    visivel BOOLEAN NOT NULL DEFAULT 0,
+    gratis BOOLEAN NOT NULL DEFAULT 0,
+    data_publicacao DATETIME,
+    duracao INT,
+    nota DECIMAL(3,2) NOT NULL DEFAULT 0,
+    preco DECIMAL(7,2) NOT NULL DEFAULT 0,
+    CONSTRAINT chk_nota CHECK(nota >= 0 AND nota <= 5)  
+    CONSTRAINT chk_preco CHECK(preco >= 0)  
+);
