a5d32802b3c9b847f7d9fbe3b2adee3db1c466cd
PedroLMaia <pedro-luiz-maia@hotmail.com>
2023-08-26 18:38:42 -0300

    WHERE com LIKE

---
+++ selects-basicos/where-like.sql
@@ -0,0 +1,27 @@
+SELECT
+    id, nome
+FROM
+    aula
+WHERE
+    nome like 'Módulo_';
+
+SELECT
+    id, nome
+FROM
+    aula
+WHERE
+    nome like 'Módulo%';
+
+SELECT
+    id, nome
+FROM
+    aula
+WHERE
+    nome like '%Módulo';
+
+SELECT
+    id, nome
+FROM
+    aula
+WHERE
+    nome like '%Módulo%';
